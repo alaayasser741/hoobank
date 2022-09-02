1ea5c737b26384a09735acb4c16ef62c780c9847
Alaa Yasser <74079336+alaayasser17@users.noreply.github.com>
2022-09-02 09:48:20 +0200

    Add States Section

--- script/main.js
+++ script/main.js
@@ -6,18 +6,30 @@ let overlay = document.querySelector(".overlay");
 if (navbarToggle) {
   navbarToggle.addEventListener("click", () => {
     sideMenu.style.left = "0";
-    overlay.style.display = 'block';
+    overlay.style.display = "block";
   });
 }
 if (closeSideMenu) {
   closeSideMenu.addEventListener("click", () => {
     sideMenu.style.left = "-105%";
-    overlay.style.display = 'none';
+    overlay.style.display = "none";
   });
 }
-if(overlay){
-    overlay.addEventListener('click',()=>{
-        sideMenu.style.left = "-105%";
-        overlay.style.display = 'none';
-    })
+if (overlay) {
+  overlay.addEventListener("click", () => {
+    sideMenu.style.left = "-105%";
+    overlay.style.display = "none";
+  });
 }
+// nav Scroll
+let myNav = document.querySelector("#nav");
+console.log(myNav)
+window.onscroll = function () {
+  if (document.body.scrollTop >= 97|| document.documentElement.scrollTop > 97) {
+    myNav.classList.add("nav-colored");
+    myNav.classList.remove("nav-transparent");
+  } else {
+    myNav.classList.add("nav-transparent");
+    myNav.classList.remove("nav-colored");
+  }
+};
